e5b3bf42c1f6cfcbfbe5266ef00fb0cd00dfdb3a
robonexx <robertwagar@gmail.com>
2024-04-07 16:30:12 +0200

    contact

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineMail } from 'react-icons/ai';
 
 import styles from './Contact.module.scss';
 
 const Contact = () => {
-  return <div className={styles.contact}>
-    <h1>Contact</h1>
-    <a target="_blank" rel="noopener noreferrer" href="mailto:marcioakaratinho@gmail.com"><AiOutlineMail /></a>
-  </div>;
+  return (
+    <div className={styles.contact}>
+      <h1 className={styles.text}>Contact</h1>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='mailto:marcioakaratinho@gmail.com'
+      >
+        <AiOutlineMail />
+      </a>
+    </div>
+  );
 };
 
 export default Contact;
