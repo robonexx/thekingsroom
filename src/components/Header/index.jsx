ea6b00ab3f50af16d67d4e2474aba55bf77d5fd5
robonexx <robertwagar@gmail.com>
2024-04-07 15:30:32 +0200

    nav

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Logo from '../Logo';
-import Nav from '../nav/Nav';
-import NavItem from '../nav/NavItem';
+import Nav from '../Nav/Nav';
+import NavItem from '../Nav/NavItem';
 import MenuBtn from '../menuBtn/MenuBtn';
 import { navData } from '../../assets/Data';
 
 const Header = () => {
   const [active, setActive] = useState(false);
   const location = useLocation();
-  
+
   useEffect(() => {
     setActive(false);
   }, [location]);
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import Logo from '../Logo/';
+import Logo from '../Logo';
 import styles from './Nav.module.scss';
 
 export default function Nav({ children, active }) {
